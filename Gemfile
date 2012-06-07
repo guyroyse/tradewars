e34c904ed871d91dbd72a9b19f054c65f908b489
Guy Royse <guy@guyroyse.com>
2012-06-06 23:01:41 -0400

    Added JSON serialization

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source :rubygems
 gem 'sinatra', '1.1.0'
 gem 'thin'
 gem 'mongo'
+gem 'json'
 
 group :test do
   gem 'rspec'
--- lib/tradewars.rb
+++ lib/tradewars.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'uri'
 require 'mongo'
+require 'json'
 
 uri = URI.parse(ENV['MONGOHQ_URL'])
 conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
@@ -8,13 +9,13 @@ db = conn.db(uri.path.gsub(/^\//, ''))
 
 sectors = db['sectors']
 
-get '/' do
-  puts 'log test'
-  sector = sectors.find_one "sector" => 1
-  sector["warps"].to_s
+get '/sector/:sector' do |num|
+  sector = sectors.find_one "sector" => num.to_i
+  sector.to_json
 end
 
 get '/bigbang' do
   sectors.remove
   sectors.insert "sector" => 1, "warps" => [2, 3, 4, 5, 6, 7]
+  "Universe generated"
 end
