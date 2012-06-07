911f7f23f565a3b471061f331fcc4afbcaf7d974
Guy Royse <guy@guyroyse.com>
2012-06-06 22:48:15 -0400

    attempt to add MongoDB

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source :rubygems
 
 gem 'sinatra', '1.1.0'
 gem 'thin'
+gem 'mongo'
 
 group :test do
   gem 'rspec'
--- lib/tradewars.rb
+++ lib/tradewars.rb
@@ -1,6 +1,20 @@
 require 'sinatra'
+require 'uri'
+require 'mongo'
+
+uri = URI.parse(ENV['MONGOHQ_URL'])
+conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
+db = conn.db(uri.path.gsub(/^\//, ''))
+
+sectors = db['sectors']
 
 get '/' do
   puts 'log test'
-  "Hello, TradeWars"
+  sector = sectors.find_one "sector" => 1
+  sector["warps"]
+end
+
+get '/bigbang' do
+  sectors.remove
+  sectors.insert "sector" => 1, "warps" => [2, 3, 4, 5, 6, 7]
 end
