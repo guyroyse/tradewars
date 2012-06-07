7a376c061581a7a36d0b6a0df5f092d14a8a7280
Guy Royse <guy@guyroyse.com>
2012-06-06 23:05:18 -0400

    reducing queried fields

--- lib/tradewars.rb
+++ lib/tradewars.rb
@@ -10,7 +10,7 @@ db = conn.db(uri.path.gsub(/^\//, ''))
 sectors = db['sectors']
 
 get '/sector/:sector' do |num|
-  sector = sectors.find_one "sector" => num.to_i
+  sector = sectors.find_one "sector" => num.to_i, :fields => ["sector", "warps"]
   sector.to_json
 end
 
