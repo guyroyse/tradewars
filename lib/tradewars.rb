2e9a771b5b5eede4aa9c66298a49aa61051ff193
Guy Royse <guy@guyroyse.com>
2012-06-06 23:42:23 -0400

    added more sectors

--- lib/tradewars.rb
+++ lib/tradewars.rb
@@ -18,5 +18,11 @@ end
 get '/bigbang' do
   sectors.remove
   sectors.insert "sector" => 1, "warps" => [2, 3, 4, 5, 6, 7]
+  sectors.insert "sector" => 2, "warps" => [1, 3, 7]
+  sectors.insert "sector" => 3, "warps" => [1, 2, 4]
+  sectors.insert "sector" => 4, "warps" => [1, 3, 5]
+  sectors.insert "sector" => 5, "warps" => [1, 4, 6]
+  sectors.insert "sector" => 6, "warps" => [1, 5, 7]
+  sectors.insert "sector" => 7, "warps" => [1, 2, 6]
   "Universe generated"
 end
