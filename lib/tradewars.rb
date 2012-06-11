0cb6de22cea05965ce2f5f73785e97e9bb50dc2a
Guy Royse <guy@guyroyse.com>
2012-06-10 23:21:47 -0400

    added index.html

--- lib/tradewars.rb
+++ lib/tradewars.rb
@@ -7,6 +7,9 @@ uri = URI.parse(ENV['MONGOHQ_URL'])
 conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
 db = conn.db(uri.path.gsub(/^\//, ''))
 
+get '/' do
+  redirect '/index.html'
+end
 
 get '/sector/:sector' do |num|
   sectors = db['sectors']
